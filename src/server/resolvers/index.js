00a7e9e02e248cf3213f9db0f03a7ec1003d2807
William Duplenne <will.dupl95@gmail.com>
2020-03-03 18:45:11 +0100

    :fire: Restart from scratch

--- src/server/resolvers/index.js
+++
@@ -1,8 +0,0 @@
-import { mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
-
-import usersResolver from './users';
-import usersType from './users/schema.graphql';
-
-export const typeDefs = mergeTypes([usersType]);
-
-export const resolvers = mergeResolvers([usersResolver]);
